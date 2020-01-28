94bd10dd0c9f2cc0799042648a1f67011c778d17
League Student <league@iMac-22.attlocal.net>
2020-01-27 18:24:48 -0800

    almost done

--- src/_02_gotta_catchem_all/ExceptionMethods.java
+++ src/_02_gotta_catchem_all/ExceptionMethods.java
@@ -1,5 +1,24 @@
 package _02_gotta_catchem_all;
 
+import java.util.Stack;
+
 public class ExceptionMethods {
-	
+
+	String reverseString(String s) {
+
+		String reverse = "";
+
+		Stack<Character> stack = new Stack<Character>();
+
+		for (int i = 0; i < s.length(); i++) {
+
+			stack.push(s.charAt(i));
+
+		}
+		for (int i = 0; i < stack.size(); i++) {
+			reverse+=stack.pop();
+		}
+		return reverse;
+
+	}
 }
